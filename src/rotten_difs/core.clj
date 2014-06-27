d89d6a29e8744559fef064c7116b3d6494ee265b
Lucas S Eggers <l.eggers91@gmail.com>
2014-06-27 20:28:40 -0300

    changed require lines to be proper and removed useless function

--- src/rotten_difs/core.clj
+++ src/rotten_difs/core.clj
@@ -1,11 +1,6 @@
 (ns rotten-difs.core
   (:require [clj-http.client :as http])
-  (:require [org.clojure/data.json :as json]))
+  (:require [clojure.data.json :as json]))
 
 (def api-key "4j3pebccyj3f9brmn8k4q4yt")
 (def ^:dynamic *base-url* "http://api.rottentomatoes.com/api/public/v1.0.json?apikey=")
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
