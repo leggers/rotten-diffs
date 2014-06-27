665b21fb38d43dc34436ba4413d97b16e5683550
Lucas S Eggers <l.eggers91@gmail.com>
2014-06-27 20:27:00 -0300

    changed requirements

--- src/rotten_difs/core.clj
+++ src/rotten_difs/core.clj
@@ -1,4 +1,9 @@
-(ns rotten-difs.core)
+(ns rotten-difs.core
+  (:require [clj-http.client :as http])
+  (:require [org.clojure/data.json :as json]))
+
+(def api-key "4j3pebccyj3f9brmn8k4q4yt")
+(def ^:dynamic *base-url* "http://api.rottentomatoes.com/api/public/v1.0.json?apikey=")
 
 (defn foo
   "I don't do a whole lot."
