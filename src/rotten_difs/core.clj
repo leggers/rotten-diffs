36f284ebeb88a2e341073ebab8daa84eb671a9fd
Lucas S Eggers <l.eggers91@gmail.com>
2014-06-27 21:06:32 -0300

    sourcing movie titles from wikipedia

--- src/rotten_difs/core.clj
+++ src/rotten_difs/core.clj
@@ -3,4 +3,30 @@
   (:require [clojure.data.json :as json]))
 
 (def api-key "4j3pebccyj3f9brmn8k4q4yt")
+(def base-wikipedia-list-url "http://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&titles=List%20of%20films:")
+(def film-list-url-endings [
+   "_numbers" "_A" "_B" "_C" "_D" "_E" "_F" "_G" "_H" "_I" "_J" "_K" "_L" "_M" "_N"
+    "_O" "_P" "_Q" "_R" "_S" "_T" "_U" "_V" "_W" "_X" "_Y" "_Z"
+   ])
 (def ^:dynamic *base-url* "http://api.rottentomatoes.com/api/public/v1.0.json?apikey=")
+
+(defn get-url
+  [url]
+  (http/get url))
+
+(defn append-api-key
+  [url]
+  (str url api-key))
+
+(defn get-base-url
+  []
+  (get-url (append-api-key *base-url*)))
+
+(defn get-base-response-body
+  []
+  (get (get-base-url) :body))
+
+(defn as-json
+  [content]
+  (json/read-str (get-base-response-body)
+                 :key-fn keyword))
