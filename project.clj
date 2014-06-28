2760c9dc72145c38bfe0ca22afb498a473f86dd1
Lucas S Eggers <l.eggers91@gmail.com>
2014-06-27 21:36:44 -0300

    added enlive for sweet html parsing

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "0.9.2"]
-                 [org.clojure/data.json "0.2.5"]])
+                 [org.clojure/data.json "0.2.5"]
+                 [enlive "1.1.5"]])
